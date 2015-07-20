70c36c1dc68fc24586a12ecaa9bce4c17b1115cd
INCBV <INCBV@NotebookPC>
2015-07-20 17:48:46 -0400

    Modificaça Fornecedor

--- src/fornecedor/teste/TesteInserir.java
+++ src/fornecedor/teste/TesteInserir.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fornecedor.teste;
+
+import fornecedor.bean.FornecedorBean;
+import fornecedor.dao.FornecedorDaoImpl;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import util.DaoException;
 
 /**
  *
- * @author INCBV
+ * @author Raildo
  */
 public class TesteInserir {
-    
+    public static void main(String[] args) {
+        FornecedorBean fornecedor = new FornecedorBean();
+        
+        fornecedor.setNome("Cooperativa Medicamentos");
+        
+        FornecedorDaoImpl fd = new FornecedorDaoImpl();
+        
+        try {
+            fd.inserir(fornecedor);
+        } catch (DaoException ex) {
+            Logger.getLogger(TesteInserir.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
